08397ce03ba9b3b899cc441c6280251f24cc71a9
feddevanderlist <feddevanderlist@gmail.com>
2017-05-27 09:42:12 +0200

    base design for forum

---
+++ src/app/template/foot.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fietser1997
+ * Date: 22-05-2017
+ * Time: 11:06
+ */
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+@include_once('app/template/head.php'); //voeg de header to
+
+
+
+@include_once('app/template/foot.php'); //voegt footer toe
